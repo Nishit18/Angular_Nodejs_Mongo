d1fe32dba027acd0905716a0d414dcdc09c59cfe
Nishit Patel <patelnishit_18@yahoo.in>
2020-05-13 23:07:03 +0530

    Add node api for get and set data

--- mean-course-angular/src/app/posts/posts.service.ts
+++ mean-course-angular/src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Post } from './post.model';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,30 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getPosts() {
-    return [...this.posts];
+    // return [...this.posts];
+    return this.http.get<{ message: string, posts: Post[] }>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
-  getPostUpdateListener(){
+  getPostUpdateListener() {
     return this.postsUpdated.asObservable();
   }
 
   addPost(title: string, content: string) {
-    const post: Post = { title, content };
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    const post: Post = { id: null, title, content };
+    this.http.post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .subscribe((response) => {
+        console.log(response.message);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+    // this.posts.push(post);
+    // this.postsUpdated.next([...this.posts]);
   }
 }
--- nodejs-api/backend/app.js
+++ nodejs-api/backend/app.js
@@ -1,14 +1,63 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('First middleware');
+// app.use((req,res,next)=>{
+//     console.log('First middleware');
+//     next();
+// });
+
+// app.use((req,res,next)=>{
+//     res.send('Hello from express!');
+// });
+
+// For convert request data in to json from stream of strings
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// For enable cors policy
+app.use((req, res, next) => {
+    res.setHeader(
+        "Access-Control-Allow-Origin",
+        "*"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PATCH, DELETE, OPTIONS, PUT"
+    );
     next();
 });
 
-app.use((req,res,next)=>{
-    res.send('Hello from express!');
+app.post('/api/posts', (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({ message: "Post added successfully" });
+});
+
+app.use('/api/posts', (req, res, next) => {
+    const posts = [
+        {
+            id: "gfgfvgkygsdfys",
+            title: "First server side post",
+            content: "This is a message"
+        },
+        {
+            id: "sdfsdfsdfsd",
+            title: "Second server side post",
+            content: "This is a message !"
+        }
+    ];
+    res.status(200).json(
+        {
+            message: "Posts fetched successfully",
+            posts: posts
+        }
+    );
 });
 
 module.exports = app;
