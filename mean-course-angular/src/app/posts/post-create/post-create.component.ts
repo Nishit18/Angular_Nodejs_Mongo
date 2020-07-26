0c0f307470e7934c38fda9cd8fae4f8f63d104f6
Nishit Patel <patelnishit_18@yahoo.in>
2020-07-26 12:45:48 +0530

    Image validation functionality

--- mean-course-angular/src/app/posts/post-create/post-create.component.ts
+++ mean-course-angular/src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { OutletContext, ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
 import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
+import { mimeType } from './mime-type.validator';
 
 @Component({
   selector: 'app-post-create',
@@ -32,7 +33,7 @@ export class PostCreateComponent implements OnInit {
       'content': new FormControl(null, {
         validators: [Validators.required]
       }),
-      'image': new FormControl(null, { validators: [Validators.required] })
+      'image': new FormControl(null, { validators: [Validators.required], asyncValidators: [mimeType] })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('postId')) {
@@ -81,7 +82,6 @@ export class PostCreateComponent implements OnInit {
     // console.log(this.form);
     const reader = new FileReader();
     reader.onload = () => {
-      debugger;
       this.imagePreview = reader.result as string;
     }
     reader.readAsDataURL(file);
